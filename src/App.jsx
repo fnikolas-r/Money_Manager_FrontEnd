d25b7031bda9d1559a368c19c4c34f8231fd040b
Nikofn20201 <nikolasfernando2001@gmail.com>
2023-05-13 02:22:34 +0800

    Add LoginbyGoogle Provider

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,15 @@ import NotFound from "./component/NotFound";
 import LoginRequired from "./component/Auth/LoginRequired";
 import {useDispatch, useSelector} from "react-redux";
 import 'react-toastify/dist/ReactToastify.css';
-import {get_rekening, get_summary_rekening} from "./storage/slices/rekening.js";
-import {get_transaksi} from "./storage/slices/transaksi.js";
-import {get_kategori} from "./storage/slices/kategori.js";
-import {get_transfer} from "./storage/slices/transfer.js";
-import {get_utangpiutang} from "./storage/slices/utang_piutang.js";
 import SignUp from "./component/SignUp.jsx";
 import {ToastContainer} from "react-toastify";
-import React, {useEffect} from "react";
-import ComingSoon from "./component/Soon";
+import React from "react";
 import Statistik from "./component/Statistik";
 import ModalMaster from "./component/MicroComponent/Modals/ModalMaster.jsx";
+import Profile from "./component/Profile.jsx";
 
 
 function App() {
-    const {isLoggedIn} = useSelector(state => state.auth);
-    const dispatch = useDispatch();
-
-    // useEffect(()=>{
-    //     if (isLoggedIn) {
-    //
-    //         dispatch(get_rekening())
-    //         dispatch(get_transaksi())
-    //         dispatch(get_kategori())
-    //         dispatch(get_utangpiutang())
-    //         dispatch(get_summary_rekening())
-    //         dispatch(get_transfer())
-    //     }
-    // },[isLoggedIn])
     return (
         <>
             <ToastContainer/>
@@ -41,7 +22,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<LoginRequired><Home/></LoginRequired>}/>
                 <Route path="/summary" element={<LoginRequired><Statistik/></LoginRequired>}/>
-
+                <Route path="/profile" element={<LoginRequired><Profile/></LoginRequired>}/>
                 <Route path="logout" element={<Login logout={true}/>}/>
                 <Route path="auth/login" element={<Login/>}/>
                 <Route path="auth/register" element={<SignUp/>}/>
